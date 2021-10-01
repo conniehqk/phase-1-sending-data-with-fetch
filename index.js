f34b5195ef1dd6b071b1e7fcd129dc94601dc595
conniekang <hqk1121@gmail.com>
2021-09-30 21:00:08 -0400

    commit tree a9e3eda92c74da3b66879f445f171cdb157bddbe

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+const userURL = 'http://localhost:3000/users'
+function submitData(username,email) {
+    return fetch(userURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email
+        })
+    }).then(resp=>resp.json()).then(json=>{
+        document.body.innerHTML = json.id
+    }).catch(error=>{
+        document.body.innerHTML = error.message
+    })
+}
